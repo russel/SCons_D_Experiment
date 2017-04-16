b6baa81c26477df7e4c0fc86332f1a4c0ecf6888
Russel Winder <russel@winder.org.uk>
2017-04-16 15:39:30 +0100

    Begin using actual builders.

--- dub.py
+++ dub.py
@@ -1,6 +1,8 @@
+from __future__ import print_function
+
 """SCons.Tool.dub
 
-An tool for integrating use of Dub with ldc, dmd, and gdc tools.
+A tool for integrating use of Dub with ldc, dmd, and gdc tools.
 
 Developed by Russel Winder (russel@winder.org.uk)
 2017-04-13 onwards.
@@ -34,100 +36,118 @@ __revision__ = "__FILE__ __REVISION__ __DATE__ __DEVELOPER__"
 import os
 import subprocess
 
-from SCons.Builder import Builder
+import SCons.Builder, SCons.Node, SCons.Errors
 
 
-def add_dub_library(env, library_name, library_version, compiler):
+class _Library(SCons.Node.FS.File):
 
-    library_directory = os.path.join(os.environ['HOME'], '.dub', 'packages', library_name + '-' + library_version, library_name)
+    def __init__(self, env, name, version):
+        self.env = env
+        self.name = name
+        self.key_name = name.replace('-', '_')
+        self.version = version
+        self.directory = os.path.join(os.environ['HOME'], '.dub', 'packages', name + '-' + version, name)
+        self.compiler = env['DC']
 
-    if not os.path.isdir(library_directory):
-            command = 'dub fetch {} --version={}'.format(library_name, library_version) if library_version is not None else 'dub fetch {}'.format(library_name)
+        if not os.path.isdir(self.directory):
+            print('Fetching', name, 'from Dub repository.')
+            command = 'dub fetch {} --version={}'.format(name, version) if version != '' else 'dub fetch {}'.format(name)
             process = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE)
             rc = process.wait()
             if rc != 0:
                 print('dub fetch returned error code', rc)
                 stderr = process.stderr.read()
                 if 'Skipping execution' not in stderr:
-                    raise Exception('Something weird happened.' + stderr)
+                    raise SCons.Errors.StopError('Something weird happened. ' + stderr)
 
-    build_directory = os.path.join(library_directory, '.dub', 'build')
-    if not os.path.isdir(build_directory):
-        process = subprocess.Popen('dub build --build=release --compiler={}'.format(compiler), shell=True, stderr=subprocess.PIPE, cwd=library_directory)
-        rc = process.wait()
-        if rc != 0:
-            print('dub build returned error code', rc)
-            stderr = process.stderr.read()
-            if 'Skipping execution' not in stderr:
-                raise Exception('Something really weird happened.' + stderr)
+        def collect_library_versions():
+            return  [f for f in os.listdir(build_directory) if f.startswith('library-release-linux.posix-x86_64-' + ('ldc' if self.compiler == 'ldc2' else self.compiler))]
 
-        if library_name == 'unit-threaded':
-            process = subprocess.Popen('dub build --build=release --compiler={} --config=gen_ut_main'.format(compiler), shell=True, stderr=subprocess.PIPE, cwd=library_directory)
+        def compile_library():
+            print('Compiling fetched', name)
+            process = subprocess.Popen('dub build --build=release --compiler={}'.format(self.compiler), shell=True, stderr=subprocess.PIPE, cwd=self.directory)
             rc = process.wait()
             if rc != 0:
                 print('dub build returned error code', rc)
                 stderr = process.stderr.read()
                 if 'Skipping execution' not in stderr:
-                    raise Exception('Something truly weird happened.' + stderr)
+                    raise SCons.Errors.StopError('Something really weird happened. ' + stderr)
 
-    selected_versions = [f for f in os.listdir(build_directory) if f.startswith('library-release-linux.posix-x86_64-' + ('ldc' if compiler == 'ldc2' else compiler))]
-    if len(selected_versions) == 0:
-        raise Exception('Library not yet built, this cannot happen.')
-    if len(selected_versions) > 1:
-        raise Exception('Multiple compiled library versions found, this cannot happen.')
-    compiled_library_directory = os.path.join(build_directory, selected_versions[0])
-    env.Append(DPATH=os.path.join(library_directory, 'source'))
-    env.Append(LIBPATH=compiled_library_directory)
-    env.Append(LIBS=library_name)
+        build_directory = os.path.join(self.directory, '.dub', 'build')
+        if not os.path.isdir(build_directory):
+            compile_library()
+        selected_versions = collect_library_versions()
+        if len(selected_versions) == 0:
+            compile_library()
+            selected_versions = collect_library_versions()
+        if len(selected_versions) > 1:
+            raise SCons.Errors.StopError('Multiple compiled library versions found, this cannot happen.')
 
-    if library_name == 'unit-threaded':
+        if name == 'unit-threaded':
+            if not os.path.isfile(os.path.join(self.directory, 'gen_ut_main')):
+                print('Recreating gen_ut_main.')
+                process = subprocess.Popen('dub build --build=release --compiler={} --config=gen_ut_main'.format(self.compiler), shell=True, stderr=subprocess.PIPE, cwd=self.directory)
+                rc = process.wait()
+                if rc != 0:
+                    print('dub build returned error code', rc)
+                    stderr = process.stderr.read()
+                    if 'Skipping execution' not in stderr:
+                        raise SCons.Errors.StopError('Something truly weird happened. ' + stderr)
 
-        def unit_threaded_make_main(env, targets, sources, library_directory=library_directory):
+            def unit_threaded_make_main(destination, directory=self.directory, env=self.env):
+                gen_ut_main = os.path.join(directory, 'gen_ut_main')
+                assert os.path.isfile(gen_ut_main), 'get_ut_main not found.'
+                rc = subprocess.call([gen_ut_main, '-f', destination])
+                if rc != 0:
+                    SCons.Errors.StopError('Failed to make ' + destination)
+                return env.File(destination)
 
-            print(env, targets, sources,library_directory)
+            setattr(self, 'UnitThreadedMakeMain', unit_threaded_make_main)
 
-            if targets is None:
-                assert len(sources) == 1
-                target_name = sources[0]
-            else:
-                assert len(targets) == 1
-                target_name = targets[0]
-            gen_ut_main = os.path.join(library_directory, 'gen_ut_main')
-            assert os.path.isfile(gen_ut_main), 'get_ut_main not found.'
-            rc = subprocess.call([gen_ut_main, '-f', target_name])
-            assert rc == 0
-            return target_name
-
-        env['BUILDERS']['UnitThreadedMakeMain'] = unit_threaded_make_main
-
-
-def unit_threaded_make_main(env, targets, sources):
-
-    print('unit_threaded_make_main', env, targets, sources)
-
-    raise Exception('Unit-Threaded is not installed.')
+        compiled_library_directory = os.path.join(build_directory, selected_versions[0])
+        self.library_file = os.path.join(compiled_library_directory, 'libunit-threaded.a')
+        env.Append(DPATH=os.path.join(self.directory, 'source'))
+        env.Append(LIBPATH=compiled_library_directory)
+        env.Append(LIBS=name)
+        SCons.Node.FS.File.__init__(self, name, env.Dir(compiled_library_directory), self)
 
 
-def ensure_dub_library_present(env, targets, sources):
+def _do_nothing(target, source, env):
+    if len(target) != 1:
+        SCons.Errors.StopError('Incorrect number of targets.')
+    if len(source) != 0:
+        SCons.Errors.StopError('Incorrect number of sources')
 
-    print('ensure_dub_library_present', env, targets, sources)
 
-    # Subvert the Builder signature protocol.
-    if targets is None:
-        assert len(sources) == 1, 'No version number and no library name.'
-        add_dub_library(env, sources[0], None, env['DC'])
-    assert len(targets) == 1, 'Incorrect number of targets.'
-    assert len(sources) == 1, 'Incorrect number of sources.'
-    add_dub_library(env, targets[0], sources[0], env['DC'])
-    return 0
+def _do_nothing_print_message(*args):
+    pass
+
+
+def _ensure_library_present_and_amend_target_path(target, source, env):
+    if len(target) != 1:
+        SCons.Errors.StopError('Incorrect number of targets.')
+    if len(source) != 1:
+        SCons.Errors.StopError('Incorrect number of sources')
+    library =  _Library(env,  target[0].name, source[0].value)
+    env.Precious(library.library_file)
+    if 'library_' + library.key_name in env:
+        print('Library already found')
+        return [], []
+    env['LIBRARIES'][library.key_name] = library
+    return [env.File(library.library_file)], []
 
 
 def generate(env):
     env['DUB'] = env.Detect('dub')
-    env.Append(BUILDERS={
-        'AddDubLibrary': ensure_dub_library_present,
-        'UnitThreadedMakeMain': unit_threaded_make_main,
-    })
+    env['LIBRARIES'] = {}
+    env['BUILDERS']['AddDubLibrary'] = SCons.Builder.Builder(
+        action=_do_nothing,
+        emitter=_ensure_library_present_and_amend_target_path,
+        target_factory=SCons.Node.FS.File,
+        source_factory=SCons.Node.Python.Value,
+        single_source=True,
+        PRINT_CMD_LINE_FUNC=_do_nothing_print_message,
+    )
 
 
 def exists(env):
